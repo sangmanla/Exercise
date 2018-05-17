ed09027cb9632023da505f3daafabd7d515f4a16
sangmanla <sangmanla@naver.com>
2018-05-17 11:40:45 -0400

    2018-05-17

--- Exercise/src/Solution.java
+++ Exercise/src/Solution.java
@@ -1,60 +1,38 @@
-import java.io.*;
-import java.util.*;
-import java.text.*;
-import java.math.*;
-import java.util.regex.*;
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.Scanner;
 
 public class Solution {
 
-	public static int equal(int[] arr) {
+    // Complete the angryChildren function below.
+    static int angryChildren(int k, int[] arr) {
+    	Arrays.sort(arr);
+    	int resultMin = Integer.MAX_VALUE;
+    	for(int i=0;i<arr.length-k+1;i++){
+    		resultMin = Math.min(resultMin, arr[i+k-1] - arr[i]);
+    	}
+    	return resultMin;
+    }
 
-	    Arrays.sort(arr);
+    private static final Scanner scanner = new Scanner(System.in);
 
-	    int sum = Integer.MAX_VALUE;
+    public static void main(String[] args) throws IOException {
 
-	    for(int base = 0; base < 3; base++) {
-	        int cSum = 0;
-	        for(int i = 0; i < arr.length; i++) {
-	            int delta = arr[i] - arr[0] + base;
-	            cSum += (int)delta / 5 + delta % 5 / 2 + delta % 5 % 2 / 1;
-	        }
-	        sum = Math.min(cSum,sum);
-	    }
+        int n = scanner.nextInt();
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
 
-	    return sum;
-	}
-	
-	public class MsLunch {
-	    private long c1 = 0;
-	    private long c2 = 0;
-	    private Object lock1 = new Object();
-	    private Object lock2 = new Object();
+        int k = scanner.nextInt();
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
 
-	    public void inc1() {
-	        synchronized(lock1) {
-	            c1++;
-	        }
-	    }
+        int[] arr = new int[n];
 
-	    public void inc2() {
-	        synchronized(lock2) {
-	            c2++;
-	        }
-	    }
-	}
-
-	public static void main(String[] args) {
-        Scanner in = new Scanner(System.in);
-        int t = in.nextInt();
-        for(int a0 = 0; a0 < t; a0++){
-            int n = in.nextInt();
-            int[] arr = new int[n];
-            for(int arr_i = 0; arr_i < n; arr_i++){
-                arr[arr_i] = in.nextInt();
-            }
-            int result = equal(arr);
-            System.out.println(result);
+        for (int i = 0; i < n; i++) {
+            int arrItem = scanner.nextInt();
+            scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+            arr[i] = arrItem;
         }
-        in.close();
+
+        System.out.println(angryChildren(k, arr));
+        scanner.close();
     }
 }
